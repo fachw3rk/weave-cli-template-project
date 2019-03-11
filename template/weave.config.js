3612cb1ef50e22eebbf096d17758612e3e795bd1
Kevin Ries <kevinries@Kevins-iMac-Pro.fritz.box>
2019-03-11 18:13:49 +0100

    Updated config file for weave 0.7.0

--- template/weave.config.js
+++ template/weave.config.js
@@ -1,26 +1,45 @@
 module.exports = {
-    // namespace: 'dev',
+    //namespace: 'dev',
     nodeId: 'my-first-node',
     logLevel: 'info',
     {{#needTransporter}}
-    transporter: '{{transporter}}',
+    transporter: {
+        adapter: '{{transporter}}',
+        // codec for transport serialization/deserialization
+        // maximum queue size
+        maxQueueSize: 80000,
+        // heartbeat interval
+        heartbeatInterval: 5 * 1000,
+        // heartbeat timeout
+        heartbeatTimeout: 10 * 1000,
+        // interval to check and remove not offline nodes.
+        offlineNodeCheckInterval: 10 * 60 * 1000
+    },
     {{/needTransporter}}
     {{#needCaching}}
     cache: '{{cache}}',
     {{/needCaching}}
     middlewares: null,
-    requestTimeout: 0 * 1000,
-    heartbeatInterval: 5000,
-    heartbeatTimeout: 1500,
-    loadBalancingStrategy: 'round_robin',
+    bulkhead: {
+        enabled: false,
+        concurrency: 15,
+        maxQueueSize: 150
+    },
     circuitBreaker: {
         enabled: false,
-        maxFailures: 3,
+        // Time after which an open circuit breaker is set to half-open.
         halfOpenTimeout: 10000,
-        failureOnTimeout: true,
-        failureOnError: true
+        maxFailures: 3,
+        windowTime: 60000
     },
-    validation: true,
+    registry: {
+        preferLocalActions: true,
+        // request timeout in ms
+        requestTimeout: 0 * 1000,
+        // loadbalancing stategy
+        loadBalancingStrategy: 'round_robin'
+    },
+    validateActionParams: true,
     metrics: {
         enabled: false,
         metricRate: 1.0
@@ -30,8 +49,8 @@ module.exports = {
         retries: 5,
         delay: 3000
     },
-    metricsRate: 1,
-    internalActions: true,
-    internalActionsAccessable: false,
+    loadNodeService: true,
+    // load Internal service actions
+    publishNodeService: false,
     watchServices: false
 }
